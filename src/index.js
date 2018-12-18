bed9d5bedcea4e57127d12cf496270cfbf0c82c1
thuitafaith <thuitamuthoni15@gmail.com>
2018-12-18 10:50:28 +0300

    add functional component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+function MyInfo() {
+    return(
+    <div>
+    <h1>Faith Thuita</h1>
+    <p>I am me</p>
+    <ul>
+        <li>The Carribean</li>
+        <li>Jamaica</li>
+    </ul>
+    </div>
+    )
+}
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
